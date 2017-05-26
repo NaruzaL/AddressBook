c5a433fd1edc25bc30253fb00a88e8d76e9be1c3
Pete Lazuran <pdlazuran@gmail.com>
2017-05-26 08:32:51 -0700

    add getters and setters to Contact.cs

---
+++ Objects/Contact.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+namespace Contact.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private string _address;
+    private int _id;
+    private static List<Contact> _instances = new List<Contact>();
+
+    public Contact (string name, string phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _instances.Add(this);
+      _id = instances.Count;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string name)
+    {
+      _name = name;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string phone)
+    {
+      _phone = phone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string address)
+    {
+      _address = address;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
