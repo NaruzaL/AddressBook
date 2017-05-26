308f975b7ee4c2a78b88b4d73e7e0442ff9e5596
Pete Lazuran <pdlazuran@gmail.com>
2017-05-26 10:27:36 -0700

    all routes to HomeModule. Fix layout of index.cshtml.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,11 +12,17 @@ using System.Collections.Generic;
           List<Contact> allContacts = Contact.GetAll();
           return View["index.cshtml"];
         };
+
         Get["/contact/add"] = _ =>  View["contact_form.cshtml"];
 
         Post["/contact/new"] = _ => {
           Contact newContact = new Contact(Request.Form["name"], Request.Form["photo"], Request.Form["phone"], Request.Form["address"]);
           List<Contact> addedContact = Contact.GetAll();
+          return View["contact_new.cshtml"];
+        };
+
+        Get[/contact/{id}] = parameters => {
+          Contact selectedContact = Contact.Find(parameters.id);
           return View["contact_new.cshtml"];
         };
 
