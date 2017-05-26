b2ef3e8c1c0a11f6aa81675323ff28ffdd14aaf2
Pete Lazuran <pdlazuran@gmail.com>
2017-05-26 08:23:10 -0700

    setup initial files, basic outline of class Contact.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Organizer.Objects;
+using System.Collections.Generic;
+
+  namespace Contact
+  {
+    public class HomeModule : NancyModule
+    {
+      public HomeModule()
+      {
+        
+      }
+    }
+  }
